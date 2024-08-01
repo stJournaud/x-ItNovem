dda332262d58b91341847f9dc7210149fa901c79
Synk <stjournaud@gmail.com>
2024-08-01 14:44:26 +0200

    added download

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,27 @@
 "use strict";
 var http = require("http");
+var fs = require("fs");
 var server = http
     .createServer(function (req, res) {
-    console.log(req);
-    res.setHeader("Content-Ype", "text/plain");
-    res.write("Try /download or /:uic");
-    res.end();
+    if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("Try /download or /:uic");
+    }
+    else if (req.url === "/download") {
+        res.writeHead(200, { "Content-Type": "text/csv" });
+        var fileStream = fs.createReadStream("./objets-trouves-restitution.csv");
+        fileStream.pipe(res);
+        fileStream.on("finish", function () {
+            res.send();
+        });
+    }
+    else {
+        res.writeHead(302, {
+            Location: "/",
+            //add other headers here...
+        });
+        res.end();
+    }
 })
     .listen(3000, "localhost", function () {
     console.log("listening on port 3000");
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http
   .createServer((req: any, res: any) => {
-    res.setHeader("Content-Ype", "text/plain");
-    res.write("Try /download or /:uic");
-    res.end();
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("Try /download or /:uic");
+    } else if (req.url === "/download") {
+      res.writeHead(200, { "Content-Type": "text/csv" });
+      const fileStream = fs.createReadStream(
+        "./objets-trouves-restitution.csv"
+      );
+      fileStream.pipe(res);
+      fileStream.on("finish", function () {
+        res.send();
+      });
+    } else {
+      res.writeHead(302, {
+        Location: "/",
+        //add other headers here...
+      });
+      res.end();
+    }
   })
   .listen(3000, "localhost", () => {
     console.log("listening on port 3000");
